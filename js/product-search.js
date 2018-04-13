98e6e0bda266cbe658a54d2765b0bfcf290fdb75
zhuo <390152513@qq.com>
2018-04-13 17:13:12 +0800

    normal update

--- js/product-search.js
+++ js/product-search.js
@@ -1,11 +1,11 @@
 function showdiv() {
 	document.getElementById("table").style.display = "inline-block";
 	document.getElementById("arrow_img").src = "images/down-arrow.png";
-	document.getElementById("arrow_a").href = "javascript:hidediv()";
+	document.getElementById("arrow_a").onclick = "hidediv(this)";
 }
 
 function hidediv() {
 	document.getElementById("table").style.display = "none";
 	document.getElementById("arrow_img").src = "images/right-arrow.png";
-	document.getElementById("arrow_a").href = "javascript:showdiv()";
+	document.getElementById("arrow_a").onclick = "showdiv(this)";
 }
