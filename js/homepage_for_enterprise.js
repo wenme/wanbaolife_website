9c03f7bdd766fd0abdff7edfc89c1fd3ee083736
zhuo <390152513@qq.com>
2018-08-21 15:56:30 +0800

    update for enterprise home page

--- js/homepage_for_enterprise.js
+++ js/homepage_for_enterprise.js
@@ -20,11 +20,15 @@ $(function() {
 		$('.business-checkbox').on('click', '.business-checkbox-pic', businessChoose);
 
 		// 关闭 contactBox
-		$('.close_contactbox_btn').click(function(event) {
+		function closeBox() {
 			$('.mask').css('display', 'none');
 			$('.contactbox').fadeOut('fast');
 			$('#contact_tel').val('');
 			$('#contact_warning_msg').text('');
+			$('.business-checkbox-pic').removeClass('remove-compare').addClass('add-compare');
+		};
+		$('.close_contactbox_btn').click(function() {
+			closeBox();
 		});
 
 		// 检查 手机号码
@@ -32,27 +36,46 @@ $(function() {
 			let tel_format = /^1\d{10}$/;
 			let tel_num = $('#contact_tel').val();
 			if ( !tel_format.test(tel_num) || tel_num.length != 11 ) {
-				$('#contact_warning_msg').text('手机号码不正确，请您重新输入。')
+				$('#contact_warning_msg').text('手机号码不正确，请您重新输入。');
+				return null;
 			}else {
-				return true;
+				return tel_num;
 			}
 		};
 		// 检查	选择业务
 		function businessTest() {
 			let business_choosed = $('.business-checkbox').find('.remove-compare');
 			if (!business_choosed.length) {
-				$('#contact_warning_msg').text('请选择至少一项业务。')
+				$('#contact_warning_msg').text('请选择至少一项业务。');
+				return null;
 			}else {
-				return true;
+				let intention = [];
+				for (let i = 0; i < business_choosed.length; i++) {
+					intention.push(business_choosed[i].textContent);
+				}
+				return intention.join();				
 			}
 		};
+
+
 		// 提交表单
 		$('#contactbtn').click(function() {
 			$('#contact_warning_msg').text('');
 			let tel_test = telTest();
 			let business_test = businessTest();
 			if (tel_test && business_test) {
-				$('#contact_warning_msg').text('我们已收到你的合作意向，将尽快联系你。');
+				let url = '/helpdesk/business_contact';
+				$.post(url, 
+					{
+						tel: tel_test,
+						intention: business_test
+					}, 
+					function() {
+						$('#contact_warning_msg').text('我们已收到你的合作意向，将尽快联系你。');
+						setTimeout(function() {
+							closeBox();
+						}, 1000);
+				});
 			}
 		});
 		
